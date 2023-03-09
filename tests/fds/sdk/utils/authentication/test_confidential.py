84714ea9827730289ba9d7f8ddb3907137e174a4
Michael Matzka <14311597+mima0815@users.noreply.github.com>
2023-03-09 12:24:10 +0100

    fix(tests): fix broken unit test

--- tests/fds/sdk/utils/authentication/test_confidential.py
+++ tests/fds/sdk/utils/authentication/test_confidential.py
@@ -177,7 +177,7 @@ def test_constructor_session_instantiation_with_additional_parameters(mocker, ex
     mock_oauth2_session = mocker.patch("fds.sdk.utils.authentication.confidential.OAuth2Session")
 
     additional_parameters = {
-        "proxy": {"http": "http://my:pass@test.test.test", "https": "http://my:pass@test.test.test"},
+        "proxies": {"http": "http://my:pass@test.test.test", "https": "http://my:pass@test.test.test"},
         "verify_ssl": False,
         "proxy_headers": {},
     }
