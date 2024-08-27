6b8a58d0c6458ae0ebfd23237a0b6701add29a8b
Filip Andonie <filip.andonie@factset.com>
2024-08-27 11:55:42 +0100

    fix: race-condition on token request close to expiry

--- src/fds/sdk/utils/authentication/confidential.py
+++ src/fds/sdk/utils/authentication/confidential.py
@@ -242,7 +242,7 @@ class ConfidentialClient(OAuth2Client):
         if not self._cached_token:
             log.debug("Access Token cache is empty")
             return False
-        if time.time() < self._cached_token[CONSTS.TOKEN_EXPIRES_AT]:
+        if time.time() < self._cached_token[CONSTS.TOKEN_EXPIRES_AT] - 30:
             return True
         else:
             log.debug("Cached access token has expired at %s", self._cached_token[CONSTS.TOKEN_EXPIRES_AT])
